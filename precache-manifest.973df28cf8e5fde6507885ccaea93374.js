c7f4a4f4dab7d1d673fb7359d082e33ff9f18395
Adam Sliwinski <sliwinski95@gmail.com>
2019-12-19 19:40:56 +0100

    Updates

---
+++ precache-manifest.973df28cf8e5fde6507885ccaea93374.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36d98a3cb8b1f012acf4b7ce9951021c",
+    "url": "/game-bet/index.html"
+  },
+  {
+    "revision": "daafd5f2786d395a85d6",
+    "url": "/game-bet/static/css/main.32c5c161.chunk.css"
+  },
+  {
+    "revision": "9e1d9b542860d1bd92bc",
+    "url": "/game-bet/static/js/2.bd4f691b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/game-bet/static/js/2.bd4f691b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "daafd5f2786d395a85d6",
+    "url": "/game-bet/static/js/main.36bf0034.chunk.js"
+  },
+  {
+    "revision": "04ef6b83f7ead240f613",
+    "url": "/game-bet/static/js/runtime-main.847c4241.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/game-bet/index.html/precache-manifest.9ee3dd731a008bb287f83881d2e3fb67.js"
+  "/game-bet/precache-manifest.973df28cf8e5fde6507885ccaea93374.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/game-bet/index.html/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/game-bet/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
